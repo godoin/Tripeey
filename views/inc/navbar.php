ae93a28e7381e81f3faf1369f4ed3af8944afbd2
Godwin Duliente <rualgodwin@gmail.com>
2024-08-21 14:40:31 -0700

    revamped navbar

--- views/inc/navbar.php
+++ views/inc/navbar.php
@@ -1,3 +1,37 @@
-<navbar>
-    tests
-</navbar>
+<nav>
+    <div class="left-content">
+        <div class="logo-wrapper">
+            <a href="/" aria-label="Homepage">
+                <img src="assets/images/logo-with-name.png" class="logo" alt="Tripeey">
+            </a>
+        </div>
+        <ul class="nav-items">
+            <li><a href="#men">Men</a></li>
+            <li><a href="#women">Women</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#categories">Categories</a></li>
+            <li><a href="#support">Support</a></li>
+        </ul>
+    </div>
+    <div class="right-content">
+        <ul class="icon-list">
+            <li>
+                <a href="/search" aria-label="Search">
+                    <img src="assets/images/icons/search.svg" alt="Search" width="20">
+                </a>
+            </li>
+            <li>
+                <a href="/cart" aria-label="Shopping Cart">
+                    <img src="assets/images/icons/cart.svg" alt="Cart" width="20">
+                </a>
+            </li>
+        </ul>
+        <a href="/login" class="ghost login" aria-label="Log In">
+            <!-- <img src="assets/images/icons/user.svg" alt="User" width="20"> -->
+            <span>LOG IN</span>
+        </a>
+        <button class="ghost menu">
+            <img src="assets/images/icons/menu.svg" alt="Menu" width="20">
+        </button>
+    </div>
+</nav>
--- views/master.php
+++ views/master.php
@@ -14,8 +14,10 @@ include_once 'core/functions.php';
   
   <title><?php echo $title; ?></title>
 
-  <link rel="stylesheet" href="assets/css/global.css" type="text/css">
-  <link rel="stylesheet" href="assets/css/index.css" type="text/css">
+  <link rel="stylesheet" href="assets\css\global.css" type="text/css">
+  <link rel="stylesheet" href="assets\css\layout.css" type="text/css">
+  <link rel="stylesheet" href="assets\css\common.css" type="text/css">
+  <link rel="stylesheet" href="assets\css\index.css" type="text/css">
   <script src="https://kit.fontawesome.com/84e2199ce0.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="assets\fontawesome-web\css\all.css">
 </head>
