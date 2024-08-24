e84a623cb43a3278bfc4bb57ddf9adcb7b77852e
Godwin Duliente <rualgodwin@gmail.com>
2024-08-24 01:41:59 -0700

    responsive landing page

---
+++ views/_index.php
@@ -0,0 +1,232 @@
+<main>
+    <!-- Hero Section -->
+    <section class="hero">
+        
+        <header>
+            <h1>Trippy Trends</h1>
+            <a href="#new" class="group" title="Explore our latest">
+                <span>New Arrivals</span>
+                <img src="assets/images/icons/narrow-right.svg" alt="Navigate to New Arrivals">
+            </a>
+        </header>
+
+        <div class="content">
+
+            <a href="#sweatshirts" class="card" title="Explore our latest collection of sweatshirts.">
+                <div class="image-container">
+                    <img src="assets/images/model/sweatshirt_model.png" class="model_img" alt="Model Wearing Sweatshirt">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Sweatshirt Details">
+                        </span>
+                    </div>
+                </div>
+                <div class="description">
+                    <h3>Sweatshirts</h3>
+                    <p>Discover stylish and comfy sweatshirts for every season.</p>
+                </div>
+            </a>
+
+            <a href="#latest" class="card main" title="Check out our latest fashion collection.">
+                <div class="image-container">
+                    <img src="assets/images/model/trenddy_model.png" class="model_img" alt="Model Wearing Latest Fashion">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Sweatshirt Details">
+                        </span>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#blouses" class="card" title="Explore our fashionable blouses.">
+                <div class="image-container">
+                    <img src="assets/images/model/blouse_model.png" class="model_img" alt="Model Wearing Stylish Blouse">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Blouse Details">
+                        </span>
+                    </div>
+                </div>
+                <div class="description">
+                    <h3>Blouses</h3>
+                    <p>Find elegant blouses to complement your wardrobe.</p>
+                </div>
+            </a>
+
+        </div>
+    </section>
+
+    <!-- Products Section -->
+    <section class="products">
+
+        <header>
+
+            <div class="group">
+                <div class="description">
+                    <h2>Our Products</h2>
+                    <p>Explore our diverse selection of stylish shirts shipped worldwide, perfect for every fashion enthusiast.</p>
+                </div>
+                <div class="actions">
+                    <a href="#allproducts" class="button primary">View All Products</a>
+                </div>
+            </div>
+
+            
+        </header>
+        
+        <nav>
+            <ul class="categories">
+                <li><a href="#" class="button outline" title="Shop T-Shirts">T-Shirts</a></li>
+                <li><a href="#" class="button outline" title="Shop Tank Tops">Tank Tops</a></li>
+                <li><a href="#" class="button outline" title="Shop Blouses">Blouses</a></li>
+                <li><a href="#" class="button outline" title="Shop Polo Shirts">Polo Shirts</a></li>
+                <li><a href="#" class="button outline" title="Shop Sweatshirts">Sweatshirts</a></li>
+                <li><a href="#" class="button outline" title="Shop Hoodies">Hoodies</a></li>
+                <li><a href="#" class="button outline" title="Shop Crop Tops">Crop Tops</a></li>
+                <li><a href="#" class="button outline" title="Shop Graphic Tees">Graphic Tees</a></li>
+                <li><a href="#" class="button outline" title="Shop Henley Shirts">Henley Shirts</a></li>
+                <li><a href="#" class="button outline" title="Shop Long-sleeves">Long-sleeves</a></li>
+            </ul>
+        </nav>
+
+        <div class="content">
+
+            <a href="#polo" class="card" title="Explore our Polos">
+                <div class="image-container">
+                    <img src="assets/images/model/polo_model.png" class="model_img" alt="Model Wearing Polo">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Polo Details">
+                        </span>
+                    </div>
+                </div>
+                <div class="description">
+                    <h3>FCB Victory Limited Polo</h3>
+                    <span class="price">PHP 1199.99</span>
+                </div>
+            </a>
+
+            <a href="#hoodie" class="card" title="Explore our Hoodies">
+                <div class="image-container">
+                    <img src="assets/images/model/hoodie_model.png" class="model_img" alt="Model Wearing Hoodie">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Hoodie Details">
+                        </span>
+                    </div>
+                </div>
+                <div class="description">
+                    <h3>Navy Horizon Hoodie</h3>
+                    <span class="price">PHP 799.99</span>
+                </div>
+            </a>
+
+            <a href="#long-sleeve" class="card" title="Explore our Long-sleeves">
+                <div class="image-container">
+                    <img src="assets/images/model/long_sleeves_model.png" class="model_img" alt="Model Wearing Long-sleeve">
+                    <div class="top-position">
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Long-sleeve Details">
+                        </span>
+                    </div>
+                </div>
+                <div class="description">
+                    <h3>Emerald Long Sleeve Tee</h3>
+                    <span class="price">PHP 799.99</span>
+                </div>
+            </a>
+
+        </div>
+
+    </section>
+
+    <!-- Styles Section -->
+    <section class="styles">
+
+        <header>
+
+            <div class="group">
+                <h2>Your Style, Your Choice: Explore Our Diverse Collection</h2>
+                <div class="description">
+                    <p>Explore our diverse selection of stylish shirts shipped worldwide, perfect for every fashion enthusiast.</p>
+                    <div class="actions">
+                        <a href="#allproducts" class="button primary">View Product Styles</a>
+                    </div>
+                </div>
+            </div>
+
+            
+        </header>
+
+
+        <div class="content">
+
+            <a href="#business" class="card" title="Explore our Business Styles">
+                <div class="image-container">
+                    <img src="assets/images/model/business_model.png" class="model_img" alt="Model Wearing Business Clothing">
+                    <div class="top-position">
+                        <div class="button badge">Business</div>
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Business Styles">
+                        </span>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#casual" class="card" title="Explore our Casual Styles">
+                <div class="image-container">
+                    <img src="assets/images/model/casual_model.png" class="model_img" alt="Model Wearing Casual Clothing">
+                    <div class="top-position">
+                        <div class="button badge">Casual</div>
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Casual Styles">
+                        </span>
+                    </div>
+                </div>
+            </a>
+            
+            <a href="#formal" class="card" title="Explore our Formal Styles">
+                <div class="image-container">
+                    <img src="assets/images/model/formal_model.png" class="model_img" alt="Model Wearing Formal Clothing">
+                    <div class="top-position">
+                        <span class="button badge">Formal</span>
+                        <span class="icon redirect">
+                            <img src="assets/images/icons/arrow-right-up.svg" alt="View Formal Styles">
+                        </span>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+    </section>
+
+    <!-- CTA Section -->
+    <section class="cta">
+
+        <header>
+            <h2>Take Your Fashion to the Next Level with Tripeey</h2>
+        </header>
+
+
+        <div class="content">
+
+            <img src="assets/images/model/sweatshirt_model.png" class="model_img" alt="Model Wearing Sweatshirt">
+
+            <div class="bottom-position">
+                <div class="tabs">
+                    <span class="tab"></span>
+                    <span class="tab"></span>
+                    <span class="tab"></span>
+                    <span class="tab"></span>
+                </div>
+                <div class="actions">
+                    <a href="" class="button primary">Start Shopping</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+</main>
